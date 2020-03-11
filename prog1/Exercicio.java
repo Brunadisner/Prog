1ea005b014f06dd5ce17bedbae87d95d65e8d9aa
Brunadisner <51202667+Brunadisner@users.noreply.github.com>
2020-03-11 15:24:40 -0300

    Add files via upload

---
+++ prog1/Exercicio.java
@@ -0,0 +1,15 @@
+public class Exercicio{
+  public static void main (String args[]){
+
+    Data d;
+    d = new Data();
+    d.dia = 11;
+    d.mes = 3;
+    d.hora = 12;
+    d.minuto = 45;
+    d.segundo = 3;
+    d.imprimi();
+    d.imprimi_hora();
+
+  }
+}
